10ef1974106c38d94b14315f95f1dfecf468b64d
AASHU7688 <tiwariaashutosh70@gmail.com>
2025-08-13 09:30:54 +0530

    This is v3 demo.py

--- demo.py
+++ demo.py
@@ -7,3 +7,7 @@ print("The sum of", a, "and", b, "is:", sum)
 #program to print the difference of two numbers
 diff = a - b
 print("The difference of", a, "and", b, "is:", diff)
+
+# program to print the product of two numbers
+product = a * b
+print("The product of", a, "and", b, "is:", product)
