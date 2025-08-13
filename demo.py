fa08fc109ab3ae29720384538dde198910c3ceff
AASHU7688 <tiwariaashutosh70@gmail.com>
2025-08-13 09:28:36 +0530

    This is v2 demo.py

--- demo.py
+++ demo.py
@@ -3,3 +3,7 @@ a=int(input("Enter first number: "))
 b=int(input("Enter second number: "))
 sum=a+b
 print("The sum of", a, "and", b, "is:", sum)
+
+#program to print the difference of two numbers
+diff = a - b
+print("The difference of", a, "and", b, "is:", diff)
